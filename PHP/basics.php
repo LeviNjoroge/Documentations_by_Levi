78ee90e358a6a1ef51b299bbaab50b387d5d0988
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-01 22:35:39 +0300

    Working on PHP Documentation: Basics

--- PHP/basics.php
+++ PHP/basics.php
@@ -5,8 +5,20 @@
         1. Let others understand your code
         2. Remind yourself of what you did
         3. Leave out some parts of your code (Mainly during testing)
+    
+    There are two types of comments; 
+        1. Multiline - Covers many lines, like the commenting on this.
+                        uses /* and */
+                        this can also be used in the middle of a statement
+        2. Singleline - covers only the line that is involved
+                        uses // before the comment
 
     Outputs in php can be done using either "echo" which is the most common
     or using "print".
 -->
 
+<?php
+    //simple "hello world statement
+    echo "Hello World!";
+
+?>
