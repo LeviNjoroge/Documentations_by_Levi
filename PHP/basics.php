c9cf8077dcbc7e881797380b446a2bd10222e405
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-01 22:31:47 +0300

    Working on PHP Documentation: Basics

--- PHP/basics.php
+++ PHP/basics.php
@@ -5,5 +5,8 @@
         1. Let others understand your code
         2. Remind yourself of what you did
         3. Leave out some parts of your code (Mainly during testing)
+
+    Outputs in php can be done using either "echo" which is the most common
+    or using "print".
 -->
 
