b979a599a03efcf23c10f0f3f4fe0fbf450db805
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-01 22:30:05 +0300

    Working on PHP Documentation: Basics

--- PHP/basics.php
+++ PHP/basics.php
@@ -3,6 +3,7 @@
 
     Comments are usually added for the following reasons:
         1. Let others understand your code
-        2. 
+        2. Remind yourself of what you did
+        3. Leave out some parts of your code (Mainly during testing)
 -->
 
