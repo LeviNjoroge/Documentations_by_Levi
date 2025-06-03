16058752cda4dcb51e0fa3eacff5163be8fb0c0b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-03 22:03:36 +0300

    Working on PHP Documentation: Variables and Data Types

--- PHP/2_variables_n_datatypes.php
+++ PHP/2_variables_n_datatypes.php
@@ -8,6 +8,10 @@
     // and just like that you have a variable
 
     // let us explore some of the datatypes available:
-    $name = "Joe"; //string
-    $age = 20; //
+    $name = "Joe"; //string ... note that the value must be in quotations, either double or single is fine
+    $age = 20; //integer
+    $gpa = 4.0; //Double/float
+    $isStudent = true; //boolean => used in conditional testing not in outputs (true=1, false=0)
+    $subjects = array("Math", "English", "Swahili", "Social Studies", "Science", "RE"); //arrays => there are three types of arrays, we are going to look into them in details later
+    $student 
 ?>
