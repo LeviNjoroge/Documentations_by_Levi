ba0d3b78aa84c35c54698974920454dac955ff30
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-03 21:56:16 +0300

    Working on PHP Documentation: Variables and Data Types

--- PHP/2_variables_n_datatypes.php
+++ PHP/2_variables_n_datatypes.php
@@ -3,5 +3,9 @@
     // but the fact that PHP is a loosely typed language,
     // means that it is not strict on the data types while declaring and assigning variables
 
+    // to declare a variable, just assign a value to a variable name:
+    $age = 20;
+    // and just like that you have a variable
+
     
 ?>
