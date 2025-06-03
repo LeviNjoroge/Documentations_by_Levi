f2d2831328686fc44f0239eb3f388ba37f97ccdd
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-03 21:52:24 +0300

    Working on PHP Documentation: Variables and Data Types

--- PHP/variables_n_datatypes.php
+++ PHP/variables_n_datatypes.php
@@ -1,3 +1,7 @@
 <?php
+    // there are many data types that PHP can handle, 
+    // but the fact that PHP is a loosely typed language,
+    // means that it is not strict on the data types while declaring and assigning variables
 
+    
 ?>
