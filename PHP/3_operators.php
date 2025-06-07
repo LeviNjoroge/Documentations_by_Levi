fcdf40f6a288ebb95a24958ee3b32ac2462b50b8
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-07 13:57:58 +0300

    Working on PHP Documentation: Operators

--- PHP/3_operators.php
+++ PHP/3_operators.php
@@ -1,5 +1,15 @@
 <?php
     // in PHP we have many categories of operators, including but not limited to:
     // arithmetic: +, -, *, /, %, **
-    
+    $x = 6;
+    $y = 7;
+
+    echo $x + $y."<br>"; // sum/addition
+    echo $x - $y."<br>"; // subtraction
+    echo $x * $y."<br>"; // multiplication
+    echo $x / $y."<br>"; // division
+    echo $x % $y."<br>"; // modulo
+    echo $x ** 2,"<br>"; // exponential
+
+    // 
 ?>
