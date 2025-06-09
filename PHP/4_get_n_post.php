e49b12ce49cb60e66359d709cd31f512b5c9f7d6
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-09 15:29:10 +0300

    Working on PHP Documentation::  and

--- PHP/4_get_n_post.php
+++ PHP/4_get_n_post.php
@@ -1,5 +1,5 @@
 <?php
     // $_GET and $_POST are super variables used to store data as it is passed from forms
     // to use them, use the subsequent methods in the forms using HTML, 
-    // and the values you are passing will be stored as items in an array
+    // and the values you are passing will be stored as items in an array  
 ?>
