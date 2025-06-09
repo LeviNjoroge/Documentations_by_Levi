bb0839de2e4f03d52f7ec45a0a5cfbaf13c1e9b4
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-09 15:29:58 +0300

    Working on PHP Documentation::  and

--- PHP/4_get_n_post.php
+++ PHP/4_get_n_post.php
@@ -3,3 +3,15 @@
     // to use them, use the subsequent methods in the forms using HTML, 
     // and the values you are passing will be stored as items in an array  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$_GET and $_POST</title>
+</head>
+<body>
+    
+</body>
+</html>
