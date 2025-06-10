922c6f92f6087b18ab910cc8241f33c2c32d858d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-10 17:55:05 +0300

    Working on PHP Documentation: form methods

--- PHP/4_get_n_post.php
+++ PHP/4_get_n_post.php
@@ -21,7 +21,7 @@
         <form action="" method="get">
             <input type="text" name="name" id="name"> <br>
             <input type="text" name="email" id="email"> <br>
-            <textarea name="message" id="message"></textarea>
+            <textarea name="message" id="message"></textarea> <br>
             <input type="submit" value="Submit">
         </form>
     </div>
