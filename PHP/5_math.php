330b8d204b49f86108d5186a77cca224f759850c
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-15 21:22:37 +0300

    Working on PHP Documentation: math functions

--- PHP/5_math.php
+++ PHP/5_math.php
@@ -2,3 +2,15 @@
 // with the guidance of "Bro Code", the youtube channel (which if you haven't discovered it yet then my advice to you is to enrol to the Youtube university soonest!! -- coz you are missing out bro! ), 
 // I will be getting user input and then perform some math on that
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
