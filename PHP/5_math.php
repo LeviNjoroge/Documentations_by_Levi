fc181ea92a0c04c1b045b4388d394584a02ef669
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-15 21:22:06 +0300

    Working on PHP Documentation: math functions

--- PHP/5_math.php
+++ PHP/5_math.php
@@ -1,3 +1,4 @@
 <?php
-// with the guidance of "Bro Code", the youtube channel (which if you haven't discovered it yet then my advice to you is to enrol to the Youtube university soonest!!)
+// with the guidance of "Bro Code", the youtube channel (which if you haven't discovered it yet then my advice to you is to enrol to the Youtube university soonest!! -- coz you are missing out bro! ), 
+// I will be getting user input and then perform some math on that
 ?>
