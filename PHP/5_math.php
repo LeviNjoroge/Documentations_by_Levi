22eaa22f0774265c78289d5a4d09423f1604a714
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-15 21:20:38 +0300

    Working on PHP Documentation: math functions

--- PHP/5_math.php
+++ PHP/5_math.php
@@ -1,3 +1,3 @@
 <?php
-// with the guidance of "Bro Code", the youtube channel (which if you haven't discovered it yet then my advice to you is to )
+// with the guidance of "Bro Code", the youtube channel (which if you haven't discovered it yet then my advice to you is to enrol to the Youtube university soonest!!)
 ?>
