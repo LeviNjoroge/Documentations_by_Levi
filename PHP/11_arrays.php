c451b65c2c34950624f9a43ebf223e33c87a29ea
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-17 17:36:44 +0300

    Working on PHP Documentation: arrays

--- PHP/11_arrays.php
+++ PHP/11_arrays.php
@@ -3,5 +3,31 @@
 // yk the way variables are containers that store single values,
 // now arrays store multiple values of the same type in a single variable
 
+// there are different types of arrays:
+// 1. indexed arrays
+// 2. associative arrays
+// 3. multidimensional arrays
+
+// indexed arrays are the common type, the array items/ values in them arrays are represented by a number(index) starting 0
+// examples of the different ways of defining the arrays;
+$marks1 = array(89,23,83);
+// OR
+$marks2[0] = 89;
+$marks2[1] = 23;
+$marks2[2] = 83;
+
+// associative arrays;
+// this is an interesting bunch of arrays, 
+// why would I say this? stay with me now...
+// yk the way array values are in "key and value" pairs, and the keys are usually indexed automatically?
+// here, you can define both your keys and values!
+// they have their indexes as strings to establish strong association btwn key and values.
+// names/keys can be ASSOCIATED with each array element using '=>' symbol
+// examples? (thought you'd never ask;) )
+$marks0 = array("Austin"=>98, "")
+
+// multidimensional arrays are also called arrays within arrays
+// they store tabular data in a single array
+// they can be represented in form of a matrix, ie in rows and columns
 
 ?>
