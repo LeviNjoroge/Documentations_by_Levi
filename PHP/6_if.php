86079b21aa5a8d7654411f81970af167979b79cf
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-17 05:49:26 +0300

    Working on PHP Documentation: if statements

--- PHP/6_if.php
+++ PHP/6_if.php
@@ -38,7 +38,7 @@ else{
 // and it exists, having an if statement with it, the condition in it is also not met, 
 // this checking through conditions will happen sequentially untill there is an else statement that is not conditional
 
-// another thing, you can 
+// one final thing, lets observe sth:
 $isAdult = true;
 $marksMath = 80;
 $marksEng = null;
@@ -59,4 +59,10 @@ if($marksEng){
 if($marksGeo){
     echo "Suspect has his geo results! <br>";
 }
+
+// from the output of the above code, you can note as follows:
+// if a variable stores a boolean value, the variable can be used as a condition
+// booleans store values as follows; false = 0, true = 1
+// hence when a variable has 0 or null, then the boolean equivalent of it is false
+// 1 or a higher number results in a true
 ?>
