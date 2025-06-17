5b221e9e620591e3c7078c0c66466f2a35301801
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-17 05:04:10 +0300

    Working on PHP Documentation: if statements

--- PHP/6_if.php
+++ PHP/6_if.php
@@ -4,5 +4,14 @@
 // executes a block of code if the condition is true
 
 // syntax:
-// if
+/* 
+    if(condition){
+        // statements/block of code
+    }
+*/
+
+// example:
+if(7>6){
+    echo "Did you know that 7 is greater than 6?";
+}
 ?>
