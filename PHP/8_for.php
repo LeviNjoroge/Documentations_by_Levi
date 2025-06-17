f345ddc326b2ed19f446aa8934c7e49278de1ffc
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-17 06:23:52 +0300

    Working on PHP Documentation: for loop

--- PHP/8_for.php
+++ PHP/8_for.php
@@ -1,3 +1,17 @@
 <?php
 // for loops
+// here we repeat a block of code a certain amount of times
+
+//syntax
+/*
+for ($i=0; $i < ; $i++) { 
+    # code...
+}
+*/
+
+// within the parenthesis following the for clause, the statements are as follows:
+// (initialization; condition; increment)
+// this is simply to mention the number of times the for loop will run
+
+//
 ?>
