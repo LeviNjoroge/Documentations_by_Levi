bfcead82d0402eac384713466bdf163d71f7dd96
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-17 08:49:20 +0300

    Working on PHP Documentation: jumping

--- PHP/10_jump.php
+++ PHP/10_jump.php
@@ -1,3 +1,13 @@
 <?php
+// in php as with other structural languages, you can jump
+// to jump is to alter normal control flow of the loops.
+
+// this can be used with any looping statement: while and for
+
+// there are two jump statements:
+// 1. break
+// 2. continue
+
+// break teminates the loop, the iteration is completed
 
 ?>
