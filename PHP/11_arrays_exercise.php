fb6511d0714ee453e13ca223cb35fc7e351b1e84
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-20 16:41:37 +0300

    Working on PHP Documentation: arrays

--- PHP/11_arrays_exercise.php
+++ PHP/11_arrays_exercise.php
@@ -2,6 +2,23 @@
 $countries = array(
     "USA" => "Washington D.C.",
     "UK" => "London",
-    "Kenya" => "Nairobi"
+    "Kenya" => "Nairobi",
+    "Canada" => "Ottawa",
+    "Germany" => "Berlin",
+    "France" => "Paris",
+    "Italy" => "Rome",
+    "Brazil" => "Brasília",
+    "Japan" => "Tokyo",
+    "China" => "Beijing",
+    "India" => "New Delhi",
+    "Russia" => "Moscow",
+    "Australia" => "Canberra",
+    "South Africa" => "Pretoria",
+    "Nigeria" => "Abuja",
+    "Egypt" => "Cairo",
+    "Mexico" => "Mexico City",
+    "Spain" => "Madrid",
+    "Norway" => "Oslo",
+    "Argentina" => "Buenos Aires"
 );
 ?>
