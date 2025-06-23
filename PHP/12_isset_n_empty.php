7e58c25f071cdac7f791d03a52d3ec592ac0c816
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-23 17:57:57 +0300

    Working on PHP Documentation: isset n empty

--- PHP/12_isset_n_empty.php
+++ PHP/12_isset_n_empty.php
@@ -1,5 +1,8 @@
 <?php
     // isset and empty, used to figure out if html elements were filled in or intereacted with
     // isset() returns true if a variable is declared and not null
-    // empty() return true if a variable is not declared, null, 
+    // empty() return true if a variable is not declared, null, false, ""
+
+    // example
+    
 ?>
