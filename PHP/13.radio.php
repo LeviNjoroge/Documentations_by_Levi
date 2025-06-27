452225c853f49a5fdc2c40f7dec1bcd2af13960c
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-27 23:34:16 +0300

    Working on PHP Documentation: radio

--- PHP/13.radio.php
+++ PHP/13.radio.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <form action="" method="post">
-        
+        <label for="mode">Please select a mode of payment:</label> <br>
         <input type="radio" name="mode" id="mode" value="visa"> <br>
         <input type="radio" name="mode" id="mode" value="cash"> <br>
         <input type="radio" name="mode" id="mode" value="paypal"> <br>
