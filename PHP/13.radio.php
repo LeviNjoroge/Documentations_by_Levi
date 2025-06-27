871b63be785d30bf5e240285b8f49837157d22f5
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-27 23:37:18 +0300

    Working on PHP Documentation: radio

--- PHP/13.radio.php
+++ PHP/13.radio.php
@@ -12,8 +12,8 @@
 <body>
     <form action="" method="post">
         <label for="mode">Please select a mode of payment:</label> <br>
-        <input type="radio" name="mode" id="mode" value="visa"> <br>
-        <input type="radio" name="mode" id="mode" value="cash"> <br>
+        <input type="radio" name="mode" id="mode" value="visa"> Visa<br>
+        <input type="radio" name="mode" id="mode" value="cash"> Cash<br>
         <input type="radio" name="mode" id="mode" value="paypal"> <br>
         <input type="submit" value="Submit"> <br>
     </form>
