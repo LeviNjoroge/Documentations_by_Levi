fda087ab98a120391fdec205c6ba632ab7daea83
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-28 17:54:51 +0300

    Working on PHP Documentation: checkboxes

--- PHP/14.checkboxes.php
+++ PHP/14.checkboxes.php
@@ -1,3 +1,16 @@
 <?php
+// we can read data from checkboxes too, as demonstrated in the following illustration
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
