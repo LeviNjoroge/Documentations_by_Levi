13efb3c1d2264c100344206b69ccd8dd6caa82ba
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-30 15:10:43 +0300

    Working on PHP Documentation: string functions

--- PHP/16_string_functions.php
+++ PHP/16_string_functions.php
@@ -4,7 +4,7 @@
 
 // for starters, lets make a model that we are going to use:
 $compName = "Kasuku!";
-$phrase = "put it down on paper✍📝";
+$phrase = "  put it down on paper✍📝";
 
 echo strtoupper($compName) . "<br>"; // makes all the characters uppercase
 echo strtolower($compName) . "<br>"; // makes all chars lowercase
@@ -12,5 +12,6 @@ echo ucwords($phrase) . "<br>"; // all the first letters of the words are upperc
 echo strlen($phrase) . "<br>"; // returns the length of the string
 echo strrev($compName) . "<br>"; // reverses the string
 echo str_word_count($phrase) . "<br>"; // counts the individual words in the string
-echo strpos()
+echo strpos($phrase, "down"); // searches for a character in the string and returns the character position of the first match of a match
+
 ?>
