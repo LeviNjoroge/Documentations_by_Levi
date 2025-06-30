e248b9b615c567ab33b4b31cfed68a8e80f5123d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-30 16:15:55 +0300

    Working on PHP Documentation: sanitize and validate input

--- PHP/17_sanitize_n_validate_input.php
+++ PHP/17_sanitize_n_validate_input.php
@@ -1,3 +1,19 @@
 <?php
-
+// here we learn how we can sanitize and validate input from the user 
+// that we got using the retreaval methods (GET n POST) 
+// and we clean the data to prevent SQL injection and incorrect data input
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sanitize n validate user input</title>
+</head>
+<body>
+    <form action="" method="post">
+        
+    </form>
+</body>
+</html>
