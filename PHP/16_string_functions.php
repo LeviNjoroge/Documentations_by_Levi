d3d62ef7d69008b932ba2f2987b8673a38047f26
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-30 15:18:25 +0300

    Working on PHP Documentation: string functions

--- PHP/16_string_functions.php
+++ PHP/16_string_functions.php
@@ -12,7 +12,7 @@ echo ucwords($phrase) . "<br>"; // all the first letters of the words are upperc
 echo strlen($phrase) . "<br>"; // returns the length of the string
 echo strrev($compName) . "<br>"; // reverses the string
 echo str_word_count($phrase) . "<br>"; // counts the individual words in the string
-echo strpos($phrase, "down") . "<br>"; // searches for a character in the string and returns the character position of the first match of a match
+echo strpos($phrase, " down") . "<br>"; // searches for a character in the string and returns the character position of the first match of a match
 echo trim($phrase) . "<br>"; // removes any whitespace b4 or after the string
 echo str_replace("down", "up", $phrase) . "<br>"; // replaces some characters with other specified ones
 echo substr($phrase, 5, 12) . "<br>";
