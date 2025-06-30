478735e6d0b373316a57be8dd125fb50f408cb15
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-30 14:58:53 +0300

    Working on PHP Documentation: functions

--- PHP/15_functions.php
+++ PHP/15_functions.php
@@ -1,11 +1,27 @@
 <?php
 // functions
-// a function is described as a piece of code that performs a perticular task
+// a function is described as a piece of code that performs a perticular task, 
+// can be reused as many times as one would wish just by calling the function
 // syntax:
 /*
-function FunctionName( //parameters) : Returntype {
+function FunctionName( #parameters) : Returntype {
     # code
 }
 */
-// example:
+// to call the created function:
+// FunctionName(#arguments);
+// example: (a function to add two numbers)
+function add($num1, $num2) : float{
+    $sum = $num1 + $num2;
+    return $sum;
+} // creating the function
+
+echo add(4.4,3) . "\n"; // calling the function
+echo add(5,21) . "\n";
+
+// example2: (a function to find the hypotenuse of a right angled triangle)
+function hypotenuse($x, $y){
+    return round(sqrt(($x**2)+($y**2)), 2);
+}
+echo hypotenuse(1,1)  . "\n";
 ?>
