210def0c169915285ac100ad8a48f3145ed8e0dd
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-01 08:30:51 +0300

    Working on PHP Documentation: sanitize and validate input

--- PHP/17_sanitize_n_validate_input.php
+++ PHP/17_sanitize_n_validate_input.php
@@ -13,7 +13,23 @@
 </head>
 <body>
     <form action="" method="post">
-        
+        <label for="username">Username: </label>
+        <input type="text" name="username" id="username"> <br>
+        <input type="submit" value="Submit" name="submit"> <br>
     </form>
 </body>
 </html>
+
+<?php
+if(isset($_POST["submit"])){
+    $username = $_POST["username"];
+    echo "Hello! {$username}";
+
+    // if user input sth that contains code, that will be executed, we have to ensure that that code will not be executed in the backend or frontend, 
+    // for example; if the user types in the following as their username: "<script>alert("You have a virus")</script>" 
+    // an alert box will be displayed and the normal working of our code wont go as planned... that means someone can actually hack our system
+
+    // to prevent this, we can add a filter to sanitize any user input as follows;
+    
+}
+?>
