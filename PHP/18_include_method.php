ab9772c5d0b79edb32ddd0bec5b24bf6d7f077e2
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-03 21:52:49 +0300

    Working on PHP Documentation: include method

--- PHP/18_include_method.php
+++ PHP/18_include_method.php
@@ -2,4 +2,5 @@
 // include()
 // what this does is, it copies the contents of a file (html, text, php)
 // and includes it in your php file.
+// this makes it easier to reuse some sections of your website
 ?>
