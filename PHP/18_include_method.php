50cfef05a5b45a698079516ad6c1332f4b5d3cb1
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-04 23:05:28 +0300

    Working on PHP Documentation: include method

--- PHP/18_include_method.php
+++ PHP/18_include_method.php
@@ -6,4 +6,17 @@
 // also the changes will only need to be applied in one file
 
 // lets look at some examples
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
