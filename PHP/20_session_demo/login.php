32c69292de50c69530bf235ab5ecfc2238fe12d8
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 16:32:30 +0300

    Working on PHP Documentation: session

---
+++ PHP/20_session_demo/login.php
@@ -0,0 +1,18 @@
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form action="" method="post">
+        Username: <input type="text" name="username" id="username"> <br>
+        Password: <input type="password" name="password" id="password"> <br>
+        <input type="submit" value="login" name:"login"> <br>
+    </form>
+</body>
+</html>
