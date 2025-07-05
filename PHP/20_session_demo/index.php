39dfbbeae3d40f07d28b163a3ed1ddbb7355f083
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 17:00:58 +0300

    Working on PHP Documentation: session

--- PHP/20_session_demo/index.php
+++ PHP/20_session_demo/index.php
@@ -9,6 +9,17 @@ session_start();
     <title>Home</title>
 </head>
 <body>
-    <h1><?php $_SESSION["username"]?></h1>
+    <h1>Hello <?php echo empty($_SESSION["username"])?"Guest":$_SESSION["username"] ?></h1>
+    <p>This is a model website to show you how to use the session</p>
+    <p>Evidently, the information is persistent as long as we maintain the session!</p>
+    <form action="" method="get">
+        <input type="submit" value="logout" name="logout"> <br>
+    </form>
 </body>
 </html>
+<?php
+if (isset($_GET["logout"])) {
+    session_abort();
+    header("Location: login.php");
+}
+?>
