370e94e016c4154d5fe5647a5a5937239f7e0910
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 16:44:09 +0300

    Working on PHP Documentation: session

--- PHP/20_session_demo/index.php
+++ PHP/20_session_demo/index.php
@@ -1,3 +1,14 @@
 <?php
 session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1><?php $_SESSION["username"]?></h1>
+</body>
+</html>
