c5424ef4efc979801f0fb41121cc5e59bc72a03d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 16:29:51 +0300

    Working on PHP Documentation: session

--- PHP/20_session.php
+++ PHP/20_session.php
@@ -1,3 +1,9 @@
 <?php
+#SESSION
+// this is a very important Super Global Variable used to store infomation about the user to be used accross multiple pages/files
+// why is it important?
+// suppose you want to create a login/signup page for users and you want to display/access their info as long as they are logged in,
+// this is great for you since as long as the user is in session, the values stored in the $_SESSION super variable will apply
 
+// to illustrate this, I will create a login and home page
 ?>
