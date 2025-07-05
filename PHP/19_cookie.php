efdf741d75e9d5c1f32c0b6f1076ee70cad67a25
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 15:03:09 +0300

    Working on PHP Documentation: cookies

--- PHP/19_cookie.php
+++ PHP/19_cookie.php
@@ -1,4 +1,10 @@
 <?php
 // COOKIES - these information about a user stored in their wen browser
 // the information can include browser preferences and other non-sensitive data
+
+// cookies are stored as key/value pairs in the $_COOKIE supervariable
+
+// to create a cookie, use the setcookie() method:
+setcookie("fav_book", "The Whale Rider", time()+(82400*7)); 
+// I have created a cookie by the name fav_book
 ?>
