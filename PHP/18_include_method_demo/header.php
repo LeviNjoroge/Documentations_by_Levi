7b74612b3a3e88cb91bec1b6722782a750d6e065
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 14:40:22 +0300

    Working on PHP Documentation: include method

--- PHP/18_include_method_demo/header.php
+++ PHP/18_include_method_demo/header.php
@@ -1,11 +1,7 @@
 <header>
     <h2>Welcome to my website!</h2>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About us</a></li>
-            <li><a href="contact.php">Contact us</a></li>
-        </ul>
-    </nav>
+    <a href="index.php">Home</a>
+    <a href="about.php">About us</a>
+    <a href="contact.php">Contact us</a>
 </header>
 <hr>
