a5e93bb480f6bed617fc910b787028b7dd786c50
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-05 15:16:30 +0300

    Working on PHP Documentation: cookies

--- PHP/19_cookie.php
+++ PHP/19_cookie.php
@@ -4,7 +4,34 @@
 
 // cookies are stored as key/value pairs in the $_COOKIE supervariable
 
-// to create a cookie, use the setcookie() method:
-setcookie("fav_book", "The Whale Rider", time()+(82400*7)); 
-// I have created a cookie by the name fav_book
+{
+    // to CREATE A COOKIE, use the setcookie() method:
+    // syntax:
+    # setcookie("key", "value", expiry_date, path_to_storage[blank for default])); 
+
+    // example:
+    setcookie("fav_book", "The Whale Rider", time()+(86400*7)); 
+    // I have created a cookie by the name fav_book with a value and the same will expire after 7days (ie 86400 seconds in a day times 7)
+
+}
+# TIP: you can view your cookies on your browser at applications>storage>cookies in your developer tools
+
+{
+    // to DELETE A COOKIE, set the expiration time to time() - 0:
+    // syntax:
+    # setcookie("key", "value", time() - 0, path_to_storage[blank for default])); 
+
+    // example:
+    setcookie("fav_book", "The Whale Rider", time()-0); 
+    // this will make the cookie disappear immediatly.
+    // you can otherwise just wait it out and it will expire on its set date
+}
+
+// since we know the location of the cookies, we can access them:
+if(isset($_COOKIE["fav_book"])){
+    echo "Your favourite book is {$_COOKIE["fav_book"]}";
+}
+else{
+    echo "I honestly do not know your fav book mate!";
+}
 ?>
