e27fe652d05a5d302d55192f179842dd7d03b718
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-07 18:20:01 +0300

    Working on PHP Documentation: PHP connect to MySQL dtb

--- PHP/23_php_X_mysql.php
+++ PHP/23_php_X_mysql.php
@@ -5,4 +5,14 @@
 # PDO (PHP Data Objects) - others call it the object oriented method.
                         // most preferred since it can connect to more than just the mysql dtb: eg oracle, postgresql, firebird, IBM, sqlite, FreeTDS, among others
 // for this, we will use the MySQL extension since the other requires knowledge of object oriented programming which is beyond the scope of this documentation
+
+// to connect to a mysql database, use the method:
+#mysqli_connect([server], [user], [password], [database_name])
+// assign this connection to a variable to create an object, this object will represent your connection
+// common practise is using "$conn" as the variable name
+// eg:
+// $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+// if the connection is successful, the variable will be declared, and thats how you can check if you have a successful connection
+
 ?>
