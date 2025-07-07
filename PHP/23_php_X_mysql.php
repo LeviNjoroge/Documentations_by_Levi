54ed6e59f07f96dedc6110b075b587ba70b87627
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-07 16:16:33 +0300

    Working on PHP Documentation: PHP connect to MySQL dtb

--- PHP/23_php_X_mysql.php
+++ PHP/23_php_X_mysql.php
@@ -2,5 +2,6 @@
 // PHP connect to MySQL database
 // PHP allows one to access and manipulate the database via two methods:
 # msqli extension
-# 
+# PDO (PHP Data Objects) - others call it the object oriented method.
+                        // most preferred since it can connect to more than just the mysql dtb: 
 ?>
