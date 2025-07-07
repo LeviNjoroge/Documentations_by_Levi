b0fa07a20a5b900bea67af8e2a1982d68ce01817
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-07 16:19:57 +0300

    Working on PHP Documentation: PHP connect to MySQL dtb

--- PHP/23_php_X_mysql.php
+++ PHP/23_php_X_mysql.php
@@ -4,4 +4,5 @@
 # msqli extension
 # PDO (PHP Data Objects) - others call it the object oriented method.
                         // most preferred since it can connect to more than just the mysql dtb: eg oracle, postgresql, firebird, IBM, sqlite, FreeTDS, among others
+// for this, we will use the MySQL extension since the other requires knowledge of object oriented programming which is beyond the scope of this documentation
 ?>
