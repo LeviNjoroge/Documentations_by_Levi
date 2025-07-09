72b781e4ac6d63bcc5fcee0d58e7153bd225598d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-09 21:01:58 +0300

    Working on PHP Documentation: PHP X MySQL: create table

--- PHP/24_php_X_mysql_create_table.php
+++ PHP/24_php_X_mysql_create_table.php
@@ -1,5 +1,5 @@
 <?php
 # creating a table on phpmyadmin
 // phpmyadmin provides a GUI to visually manipulate a table
-// 
+// to access phpmyadmin ensure mysql and apache servers are running on 
 ?>
