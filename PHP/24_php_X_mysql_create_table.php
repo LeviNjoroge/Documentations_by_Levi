0d99ea1000b0ab3f13c95425dd3528ff143418fa
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-10 22:58:46 +0300

    Working on PHP Documentation: PHP X MySQL: create table

--- PHP/24_php_X_mysql_create_table.php
+++ PHP/24_php_X_mysql_create_table.php
@@ -1,6 +1,7 @@
 <?php
 # creating a table on phpmyadmin
 // phpmyadmin provides a GUI to visually manipulate a table
-// to access phpmyadmin ensure mysql and apache servers are running on XAMPP, then proceed to this url: http://localhost/phpmyadmin
+// to access phpmyadmin ensure mysql and apache servers are running on XAMPP, then proceed to this url: http://localhost/phpmyadmin, 
+// alternatively, 
 //  
 ?>
