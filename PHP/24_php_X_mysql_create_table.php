4b34f3b97f88c62bb01c6f704a6f4eecf3f1d046
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-12 13:47:29 +0300

    Working on PHP Documentation: PHP X MySQL

--- PHP/24_php_X_mysql_create_table.php
+++ PHP/24_php_X_mysql_create_table.php
@@ -3,5 +3,6 @@
 // phpmyadmin provides a GUI to visually manipulate a table
 // to access phpmyadmin ensure mysql and apache servers are running on XAMPP, then proceed to this url: http://localhost/phpmyadmin, 
 // alternatively, use the admin button on the XAMPP server manager under MYSQL
-//  
+
+// the way of creating a database to a table and even populating is very straight forward. Id you need help, I would recommend checking out BroCode, 
 ?>
