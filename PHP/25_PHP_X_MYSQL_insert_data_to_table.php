f486df7066acb9776501e5a85f2e0c3e0f311a22
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-12 14:27:53 +0300

    Working on PHP Documentation: PHP X MySQL

--- PHP/25_PHP_X_MYSQL_insert_data_to_table.php
+++ PHP/25_PHP_X_MYSQL_insert_data_to_table.php
@@ -3,5 +3,7 @@
 // prerequisite of this is for you to have created a database and a table in it, so that we can populate it using PHP
 
 //let's get into it...
+include('database.php');
 
+mysqli_close($conn); // you should close your openned connection, this is good practice esp for resource management
 ?>
