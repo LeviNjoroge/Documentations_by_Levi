51d9cf13ea53f0b1695d677d05e99a937367ff3b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-12 14:50:21 +0300

    Working on PHP Documentation: PHP X MySQL

--- PHP/25_PHP_X_MYSQL_insert_data_to_table.php
+++ PHP/25_PHP_X_MYSQL_insert_data_to_table.php
@@ -7,9 +7,19 @@ include("database.php");
 
 $raw_sql = "INSERT INTO users(user, password) VALUES ('Dora', 'doratheexplorer')"; // store your query in a variable for clean code
 
-// the raw sql statement will work, but a tidier way of doing the same, that can give us more posibilities and 
+// the raw sql statement will work, but a tidier way of doing the same, that can give us more posibilities and more operatianality will be to use variables to store the data to be entered and pass the variables to the sql statement
+// as follows:
+$username = "fireman_sam";
+$password = "jimjam_sam";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+$insert_users_query = "
+                        INSERT INTO users(user, password)
+                        VALUES ('$username', '$hash'); 
+"; // remember to use the variable names inside single quotes
+
 try {
-    mysqli_query($conn, $raw_sql); // this is a method that takes two parameters; connection object and the query
+    mysqli_query($conn, $insert_users_query); // this is a method that takes two parameters; connection object and the query
     echo "User registered successfully";
 } catch (Exception $e) {
     echo "Could not register user!";
