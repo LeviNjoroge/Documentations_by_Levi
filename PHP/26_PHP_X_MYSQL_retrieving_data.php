fd6fd66118439a8128eee20320c6925d516b9745
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-12 15:25:09 +0300

    Working on PHP Documentation: PHP X MySQL

--- PHP/26_PHP_X_MYSQL_retrieving_data.php
+++ PHP/26_PHP_X_MYSQL_retrieving_data.php
@@ -1,3 +1,19 @@
 <?php
+include('database.php');
 // we'll be going through retrieving table data using PHP
+
+// mysqli_query() will still be used, as follows:
+
+$get_users_query = "SELECT * FROM users"; // this is our sql statement that will retreave all the users
+
+$result = mysqli_query($conn, $get_users_query); // this will return an object, hence must be stored in a variable
+
+// first, check if there are any row in the results:
+if(mysqli_num_rows($result)>0){ // we are checking if there is at least one row returned
+    while ($row = mysqli_fetch_assoc($result))
+}else {
+    echo "No user found";
+}
+
+mysqli_close($conn);
 ?>
