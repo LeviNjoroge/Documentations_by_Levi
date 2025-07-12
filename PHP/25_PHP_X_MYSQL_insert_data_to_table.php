f2f306f0694db7ce4ce664278ff472754c39f197
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-12 14:21:11 +0300

    Working on PHP Documentation: PHP X MySQL

--- PHP/25_PHP_X_MYSQL_insert_data_to_table.php
+++ PHP/25_PHP_X_MYSQL_insert_data_to_table.php
@@ -2,5 +2,6 @@
 // Inserting data into a table in a database using PHP
 // prerequisite of this is for you to have created a database and a table in it, so that we can populate it using PHP
 
-//
+//let's get into it...
+
 ?>
