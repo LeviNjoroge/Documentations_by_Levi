b51b9561b471a71f043ed3356a023faad2948d37
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-16 21:02:32 +0300

    Working on a basic PHP project

--- PHP/basic project/api.php
+++ PHP/basic project/api.php
@@ -6,7 +6,7 @@ include("database.php");
 function check_balance($id){
 
     $sql_check_balance = "SELECT balance FROM finances WHERE user_id = {$id}";
-    mysqli_query($GLOBALS["conn"], $sql_check_balance);
+    $result = mysqli_query($GLOBALS["conn"], $sql_check_balance);
 }
 
 mysqli_close($conn);
