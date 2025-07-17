5ede4c4d8ef0812b0b7a906b896017f8d609610a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-17 23:26:20 +0300

    Working on a basic PHP project

--- PHP/basic project/index.php
+++ PHP/basic project/index.php
@@ -10,7 +10,7 @@ include("api.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>la Jay Banca</title>
+    <title>Testing login</title>
 </head>
 <body>
     <h1>Hi <?php echo $first_name?>!</h1>
