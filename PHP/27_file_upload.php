59010127fd01356951edebc91c3ece65718e8cdf
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-22 08:02:04 +0300

    bonus episode: file upload

--- PHP/27_file_upload.php
+++ PHP/27_file_upload.php
@@ -11,17 +11,35 @@
 
 ?>
 <form action="" method="post" enctype="multipart/form-data">
-    <input type="file" name="file" id="file"><br>
+    <input type="file" name="file" id="file" accept=".png, .jpg, .jpeg" size="1024"><br>
     <input type="submit" value="submit" name="submit"> <br>
 </form>
 <?php
 if (isset($_POST["submit"])) {
     $file = $_FILES['file'];
-    // echo $file['name'];
-    foreach($file as $key => $value){
-        echo "{$key} = {$value} <br>";
-    }
     
+    // ensure file is of the correct extension
+    $fileName = $file['name']; // get array name
+    $fileNameParts = explode('.',$fileName); // explode the name on '.' to make the name part and the extension part
+    $fileExt = strtolower(end($fileNameParts)); // get the last element of the above array (ext part) and store in lowercase
+    
+    $accepted_extensions = array("png", "jpeg", "jpg"); // have an array of acceptable extensions
+    
+    if (in_array($fileExt, $accepted_extensions)) {
+        if ($file['error']) { // ensure no error
+            echo "There was an error uploading this file, please try again later!";
+        } else{
+            if ($file['size'] < 6001) { // restrict the file size for efficiency in storage
+                $current_file_location = $file['tmp_name'];
+                $new_file_location = "27_files_upload/" . rand(1000, 10000) . "." . $fileExt;
+                move_uploaded_file($current_file_location, $new_file_location);
+            } else {
+                echo "The file is too large! Can't be aploaded:(";
+            }
+        }
+    } else {
+        echo "Invalid file type!";
+    }
 }
 
 ?>
